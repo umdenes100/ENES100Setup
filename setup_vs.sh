5689ed9d403b840819c00689bb0e8944e049a6e6
Mukul Ashwath Ram <mukul.ram97@gmail.com>
2018-09-12 09:12:58 -0400

    Commented out Client

--- setup_vs.sh
+++ setup_vs.sh
@@ -6,27 +6,27 @@
 
 VS_LINK="https://github.com/umdenes100/VisionSystem.git"
 VSR_LINK="https://github.com/umdenes100/VisionSystemRemote.git"
-VSRC_LINK="https://github.com/umdenes100/VisionSystemRemoteClient.git"
+# VSRC_LINK="https://github.com/umdenes100/VisionSystemRemoteClient.git"
 
-apt-get update
-apt-get upgrade
+apt update
+apt upgrade
 
 mkdir -p ~/Development && cd ~/Development
 
 # Downloading repos.
 git clone $VS_LINK
 git clone $VSR_LINK
-git clone $VSRC_LINK
+# git clone $VSRC_LINK
 
 # Setting up log directories.
-cd VisionSystemRemoteClient && mkdir logs && cd logs
-touch access.log && touch error.log
+# cd VisionSystemRemoteClient && mkdir logs && cd logs
+# touch access.log && touch error.log
 
 # Setting up nginx.
-cp ./nginx_conf /etc/nginx/sites-available/VisionSystemRemoteClient
-ln -s /etc/nginx/sites-available/VisionSystemRemoteClient /etc/nginx/sites-enabled/VisionSystemRemoteClient
+# cp ./nginx_conf /etc/nginx/sites-available/VisionSystemRemoteClient
+# ln -s /etc/nginx/sites-available/VisionSystemRemoteClient /etc/nginx/sites-enabled/VisionSystemRemoteClient
 
-nginx -s reload
+# nginx -s reload
 
 # Add port to groups.
 
